f1659b14368fc658089f2c54a7e4a60c62c8f743
keverza <keverza@protonmail.com>
2022-06-11 20:53:39 +0300

    chart.js

--- src/routes/products/[product]/poll.svelte
+++ src/routes/products/[product]/poll.svelte
@@ -9,43 +9,54 @@
 	};
 
 	//Chart js example
+	const data = {
+		labels: ['Uogos', 'Riešutai', 'Vaisiai', 'Kakava', 'Kava', 'Riešutai', 'Karamelė'],
+		datasets: [
+			{
+				label: 'Jūsų vertinimas',
+				data: [65, 59, 90, 81, 56, 55, 40],
+				fill: true,
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+				borderColor: 'rgb(255, 99, 132)',
+				pointBackgroundColor: 'rgb(255, 99, 132)',
+				pointBorderColor: '#fff',
+				pointHoverBackgroundColor: '#fff',
+				pointHoverBorderColor: 'rgb(255, 99, 132)'
+			},
+			{
+				label: 'Vertinimo vidurkis',
+				data: [50, 50, 50, 50, 50, 80, 50],
+				fill: true,
+				backgroundColor: 'rgba(211, 211, 211, 0.2)',
+				borderColor: 'rgb(211, 211, 211)',
+				pointBackgroundColor: 'rgb(200,200,200)',
+				pointBorderColor: '#fff',
+				pointHoverBackgroundColor: '#fff',
+				pointHoverBorderColor: 'rgb(50, 50, 50)'
+			}
+		]
+	};
 	import { onMount } from 'svelte';
 	let myChartCanvas;
 
 	onMount(async (promise) => {
 		const ctx = myChartCanvas.getContext('2d');
 		const myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-				datasets: [
-					{
-						label: '# of Votes',
-						data: [12, 19, 3, 5, 2, 3],
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)'
-						],
-						borderColor: [
-							'rgba(255, 99, 132, 1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)'
-						],
-						borderWidth: 1
-					}
-				]
-			},
+			type: 'radar',
+			data: data,
 			options: {
+				elements: {
+					line: {
+						borderWidth: 3
+					}
+				},
 				scales: {
-					y: {
-						beginAtZero: true
+					r: {
+						angleLines: {
+							display: true
+						},
+						suggestedMin: 10,
+						suggestedMax: 100
 					}
 				}
 			}
@@ -53,12 +64,17 @@
 	});
 </script>
 
+<div class="h-20" />
 <div class="flex flex-col justify-center items-center">
 	<h2>Apklausa</h2>
 	<p>Įvertinkite <span class="font-semibold"> {curProduct[0].name}</span> šokolado skonį</p>
 </div>
 
-<div class=""><canvas bind:this={myChartCanvas} id="myChart" width="400" height="400" /></div>
+<div class="flex justify-center items-center">
+	<div class="max-w-2xl">
+		<canvas bind:this={myChartCanvas} id="myChart" width="400" height="400" />
+	</div>
+</div>
 
 <form on:submit|preventDefault={handleSubmit} class="flex flex-col justify-center items-center">
 	<label for="text"
