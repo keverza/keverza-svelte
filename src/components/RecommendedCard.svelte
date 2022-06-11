f5b5c8652b3f8bf973d781ccb1afb4e5e126fa7b
keverza <keverza@protonmail.com>
2022-06-11 18:01:31 +0300

    numbers on cart icon

--- src/components/RecommendedCard.svelte
+++ src/components/RecommendedCard.svelte
@@ -1,14 +1,18 @@
 <script>
+	import { goto, invalidate } from '$app/navigation';
+
+	import { isProductOpen } from '../stores/overlayStore';
+
 	export let product;
 	let { href, imageSrc, imageAlt, name } = product;
-	let transforms = 'ar_1:1,c_fill,g_center,h_250,w_250';
+	let imgTransforms = 'ar_1:1,c_fill,g_center,h_250,w_250';
 </script>
 
-<a target="_self" href={`/products/${href}`} class="group">
+<a href={`/products/${href}`} class="group">
 	<div class="w-full overflow-hidden rounded-sm ">
 		<div class="relative  group-hover:opacity-75   ">
 			<img
-				src={`https://res.cloudinary.com/dpbpnidgc/image/upload/${transforms}/v1653929573/kvrz/${imageSrc}`}
+				src={`https://res.cloudinary.com/dpbpnidgc/image/upload/${imgTransforms}/v1653929573/kvrz/${imageSrc}`}
 				alt={imageAlt}
 				class="h-full w-full object-cover object-center group-hover:opacity-75 aspect-square rounded-md"
 			/>
