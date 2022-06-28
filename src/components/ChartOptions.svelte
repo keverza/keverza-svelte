c52c0c6bdba9e50ae06fe8bcfb9ecbccf0b18e9f
keverza <keverza@protonmail.com>
2022-06-28 21:18:37 +0300

    poll routing uodate

--- src/components/ChartOptions.svelte
+++ src/components/ChartOptions.svelte
@@ -1,11 +1,19 @@
 <script>
 	import ChartLabelsItem from './ChartLabelsItem.svelte';
+	import { questionOptions } from '../stores/productStore';
+	import ChartRadar from './ChartRadar.svelte';
 
-	export let options;
-	export let title;
+	let currentQuestion = 0;
+	let selectedOption = 'tastes';
+	let title = 'skonius';
+	let isCompleted = false;
+
+	let data = [80, 80, 80, 80, 80, 80, 80];
+
+	const optionKeys = Object.keys($questionOptions);
 
 	function toggle(toggled) {
-		options = options.map((label) => {
+		$questionOptions[selectedOption] = $questionOptions[selectedOption].map((label) => {
 			if (label === toggled) {
 				return {
 					id: label.id,
@@ -13,26 +21,89 @@
 					selected: !label.selected
 				};
 			}
+
 			return label;
 		});
 	}
 
-	const handleSubmit = () => {
-		console.log('selected', title, options);
+	const handleNext = () => {
+		// currentQuestion += 1;
+		switch (currentQuestion) {
+			case 0:
+				selectedOption = 'tastes';
+				title = 'skonius';
+				break;
+			case 1:
+				selectedOption = 'aromas';
+				title = 'aromatus';
+				break;
+			case 2:
+				selectedOption = 'mouthfeel';
+				title = 'pojūtį burnoje';
+				break;
+			case 3:
+				selectedOption = 'mouthfeel';
+				title = 'pojūtį burnoje';
+				break;
+
+			case 4:
+				isCompleted = true;
+				console.log($questionOptions);
+				//reset store
+				$questionOptions = optionKeys.map((key) =>
+					$questionOptions[key].map((item) => {
+						return {
+							selected: false,
+							id: item.id,
+							text: item.text
+						};
+					})
+				);
+
+				console.log($questionOptions);
+				break;
+			default:
+				title = 'skonius';
+				break;
+		}
+	};
+
+	const handleForm = () => {
+		console.log('selected', currentQuestion, $questionOptions[selectedOption]);
+		handleNext();
 	};
 </script>
 
-<section class="flex flex-col justify-center items-center pt-10">
-	<h2 class="text-2xl">Pasirinkite jaučiamus <strong>{title}</strong></h2>
-	<form on:submit|preventDefault={handleSubmit} class="flex flex-col justify-center items-center">
-		<div class="grid grid-cols-4 auto-cols-max justify-items-center items-center p-5">
-			{#each options as label (label.id)}
-				<ChartLabelsItem {label} on:click={() => toggle(label)} />
-			{/each}
-		</div>
-		<button
-			class="border border-gray-800 bg-white font-semibold hover:bg-amber-500 hover:text-white rounded-lg py-1 px-3"
-			>Pateikti</button
-		>
-	</form>
+<section class="flex flex-col justify-center items-center pt-10 min-h-screen">
+	{#if !isCompleted}
+		<h2 class="text-2xl">Pasirinkite jaučiamus <strong>{title}</strong></h2>
+		<form on:submit|preventDefault={handleForm} class="flex flex-col justify-center items-center">
+			{#if currentQuestion === 3}
+				<ChartRadar {data} />
+			{:else}
+				<div class="grid grid-cols-4 auto-cols-max justify-items-center items-center p-5">
+					{#each $questionOptions[selectedOption] as label (label.id)}
+						<ChartLabelsItem {label} on:click={() => toggle(label)} />
+					{/each}
+				</div>
+			{/if}
+
+			<div>
+				{#if currentQuestion > 0}
+					<button
+						on:click={() => (currentQuestion -= 1)}
+						class="border border-gray-500 bg-white text-gray-500 font-semibold hover:bg-amber-500 hover:text-white rounded-lg py-1 px-3"
+						>Atgal</button
+					>
+				{/if}
+				<button
+					on:click={() => (currentQuestion += 1)}
+					class="border border-gray-800 bg-white  font-semibold hover:bg-amber-500 hover:text-white rounded-lg py-1 px-3"
+					>{currentQuestion > 2 ? 'Pateikti' : 'Toliau >'}</button
+				>
+			</div>
+		</form>
+	{:else}
+		<p>Atsakymas <em>'priimtas'</em> . Ačiū už atsakymus</p>
+	{/if}
 </section>
--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -6,6 +6,49 @@ export const currentProduct = writable([])
 export const filteredProduct = writable([])
 export const props = writable([])
 
+// Poll stores
+
+
+export const questionOptions =  writable({
+    tastes :[    
+        { id: 1, selected: false, text: 'Karamelė' },
+        { id: 2, selected: false, text: 'Uogos' },
+        { id: 3, selected: false, text: 'Kakava' },
+        { id: 4, selected: false, text: 'Riešutai' },
+        { id: 5, selected: false, text: 'Sirupas' },
+        { id: 6, selected: false, text: 'Dribsniai' },
+        { id: 7, selected: false, text: 'Pienas' },
+        { id: 8, selected: false, text: 'Vanilė' },
+        { id: 9, selected: false, text: 'Džiovinti' },
+        { id: 10, selected: false, text: 'Tropiniai' },
+        { id: 11, selected: false, text: 'Raudonieji' },
+        { id: 12, selected: false, text: 'Citrusiniai' },
+        { id: 13, selected: false, text: 'Žolelės' },
+        { id: 14, selected: false, text: 'Prieskoniai' },
+        { id: 15, selected: false, text: 'Gėlių' }
+        ],
+    aromas : [
+        { id: 1, selected: false, text: 'Umami' },
+        { id: 2, selected: false, text: 'Sūru' },
+        { id: 3, selected: false, text: 'Saldu' },
+        { id: 4, selected: false, text: 'Kartu' },
+        { id: 5, selected: false, text: 'Rūgštu' },
+        { id: 6, selected: false, text: 'Saldu' }
+        ],
+    mouthfeel : [
+        { id: 1, selected: false, text: 'Vėsus' },
+        { id: 2, selected: false, text: 'Šiltas' },
+        { id: 3, selected: false, text: 'Sutraukiantis' },
+        { id: 4, selected: false, text: 'Aitrus' },
+        { id: 5, selected: false, text: 'Švelnus' },
+        { id: 6, selected: false, text: 'Grubus' },
+        { id: 7, selected: false, text: 'Vaškinis' }
+        ]})
+
+// export const currentQuestionOption = readable([questionOptions.tastes, questionOptions.aromas, questionOptions.mouthfeel])
+
+
+
 // const fetchProduct = async ({fetch}) => {    
 //     const res = await fetch('/all.json')
 //     const {data} = await res.json()    
